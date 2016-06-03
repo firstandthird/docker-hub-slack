4134bcfd7040b8194ae3a6e19fbeecb1bc542c64
Greg Allen <greg@firstandthird.com>
2016-06-03 12:10:43 -0700

    wired up hub endpoint, added test endpoint to test

--- routes/hub.js
+++ routes/hub.js
@@ -5,7 +5,13 @@ exports.hub = {
   handler(request, reply) {
     const server = request.server;
 
-    server.log(['success', 'docker-hub'], 'some repo here');
+    const payload = request.payload;
+    const tag = payload.push_data.tag;
+    const repo = payload.repository.repo_name;
+    const url = payload.repository.repo_url;
+    const image = `${repo}:${tag}`;
+
+    server.log(['success', 'docker-hub', image], `${image} has been pushed. ${url}`);
 
     reply('ok');
   }
